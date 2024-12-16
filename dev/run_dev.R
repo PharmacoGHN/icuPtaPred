ff086589eed1f52b9e4858d81f3365aece84f51a
Romain Garreau <romain.garreau@chu-lyon.fr>
2024-12-16 22:45:33 +0100

    Update run_dev.R

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -9,7 +9,6 @@ options(
 
 # Detach all loaded packages and clean your environment
 golem::detach_all_attached()
-# rm(list=ls(all.names = TRUE))
 
 # Document and reload your package
 golem::document_and_reload()
