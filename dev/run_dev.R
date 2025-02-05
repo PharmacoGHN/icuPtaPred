1bca9e34642e5b44bff7214b487dfac56d7bdbb5
Romain Garreau <romain.garreau@chu-lyon.fr>
2025-02-05 15:23:28 +0100

    Update run_dev.R
    add stack trace for debugging
    add autoreload feather for UI dev

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -4,7 +4,8 @@ options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mo
 # Comment this if you don't want the app to be served on a random port
 options(
   shiny.port = httpuv::randomPort(),
-  shiny.fullstacktrace = TRUE
+  shiny.fullstacktrace = TRUE,
+  shiny.autoreload = TRUE
 )
 
 # Detach all loaded packages and clean your environment
