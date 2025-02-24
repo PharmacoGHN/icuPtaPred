0ac7fba3922a38ed3460f07eba61147f50ed29b9
Romain Garreau <romain.garreau@chu-lyon.fr>
2025-02-24 18:35:02 +0100

    Update mod_model_information.R
    update model information display

--- R/mod_model_information.R
+++ R/mod_model_information.R
@@ -2,7 +2,7 @@
 #'
 #' @description A shiny Module.
 #'
-#' @param id,input,output,session Internal parameters for {shiny}.
+#' @param id Module id.
 #'
 #' @noRd
 #'
@@ -11,19 +11,28 @@ mod_model_information_ui <- function(id) {
   ns <- NS(id)
   tagList(
     fluidPage(
-      titlePanel("Pharmacokinetic Model Information"),
-      sidebarLayout(
-        sidebarPanel(
-          selectInput(ns("model"), "Select Model:", choices = names(model_information))
+      tags$div(
+        class = "title-frame",
+        h3("Pharmacokinetic Model Information", class = "title-display")
+      ),
+      br(),
+      fluidRow(
+        column(
+          width = 3,
+          # Initially no choices: these will be updated via the server
+          selectInput(ns("drug"), "Select Drug:", choices = labels("drug", "choices", lang)),
+          selectInput(ns("model"), "Select Model:", choices = "No model currently available")
         ),
-        mainPanel(
-          h3(textOutput(ns("title"))),
+        column(
+          width = 9,
+          div(
+            class = "title-frame-section",
+            h3(textOutput(ns("title")), class = "title-display")
+          ),
+          br(),
           p(strong("Authors: "), textOutput(ns("authors"))),
-          p(strong("Year: "), textOutput(ns("year"))),
-          p(strong("Journal: "), textOutput(ns("journal"))),
-          p(strong("DOI: "), textOutput(ns("doi"))),
           p(strong("Abstract: "), textOutput(ns("abstract"))),
-          p(strong("Clearance Formula: "), textOutput(ns("clearance_formula"))),
+          p(strong("Clearance Formula: "), uiOutput(ns("clearance_formula"))),
           p(strong("Model Description: "), textOutput(ns("model_description"))),
           p(strong("Population Studied: "), textOutput(ns("population_studied")))
         )
@@ -34,29 +43,65 @@ mod_model_information_ui <- function(id) {
 
 #' model_information Server Functions
 #'
+#' @param id Module id.
+#'
 #' @noRd
 mod_model_information_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    # allow model selection to desplay information
-    selected_model <- reactive({ model_information[[input$model]] })
+    # Update the list of models when r$drug changes
+    observeEvent(input$drug, {
+      golem::cat_dev("DEBUG: input$drug is ", input$drug, "\n")
+      if (!is.null(input$drug)) {
+        updateSelectInput(session, "model", choices = names(model_information[[input$drug]]))
+      }
+    })
 
-    output$title <- renderText({ selected_model()$Title })
-    output$authors <- renderText({ selected_model()$Authors })
-    output$year <- renderText({ selected_model()$Year })
-    output$journal <- renderText({ selected_model()$Journal })
-    output$doi <- renderText({ selected_model()$DOI })
-    output$abstract <- renderText({ selected_model()$Abstract })
-    output$clearance_formula <- renderText({ selected_model()$Clearance_Formula })
-    output$model_description <- renderText({ selected_model()$Model_Description })
-    output$population_studied <- renderText({ selected_model()$Population_Studied })
+    # Choose the selected model information using r$drug and the selected model name
+    selected_model <- reactive({
+      req(input$drug, input$model)
+      model_information[[input$drug]][[input$model]]
+    })
 
+    # Update the UI with the selected model information
+    output$title <- renderText({
+      selected_model()$Title
+    })
+    output$authors <- renderUI({
+      authors <- paste0(
+        selected_model()$Authors, ", ",
+        selected_model()$Journal,
+        selected_model()$Year, ". ", "DOI: ",
+        selected_model()$DOI
+      )
+      
+
+      golem::cat_dev(authors, "\n")
+      #tagList(
+        a(authors,
+          # paste(
+          # selected_model()$Authors, ",",
+          # selected_model()$Journal,
+          # selected_model()$Year, ".", "DOI: ",
+          # selected_model()$DOI
+          # ),
+          href = "https://journals.asm.org/doi/10.1128/aac.02556-19"#,
+          #target = "_blank"
+        )
+      #)
+    })
+    output$abstract <- renderText({
+      selected_model()$Abstract
+    })
+    output$clearance_formula <- renderUI({
+      withMathJax(selected_model()$Clearance_Formula)
+    })
+    output$model_description <- renderText({
+      selected_model()$Model_Description
+    })
+    output$population_studied <- renderText({
+      selected_model()$Population_Studied
+    })
   })
 }
-
-## To be used in UI:
-# mod_model_information_ui("model_information_1")
-
-## To be used in server:
-# mod_model_information_server("model_information_1")
