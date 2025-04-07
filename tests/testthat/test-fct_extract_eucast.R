c0f4ee5cfd85454fa333870d120d665593f7cd93
Romain Garreau <romain.garreau@chu-lyon.fr>
2025-04-07 17:17:49 +0200

    Update test-fct_extract_eucast.R
    skip untill #20 is fixed

--- tests/testthat/test-fct_extract_eucast.R
+++ tests/testthat/test-fct_extract_eucast.R
@@ -1,6 +1,7 @@
 testthat::test_that("extract_eucast return the right list and is up to date", {
 
    res_update_eucast <- update_eucast()
+   skip("mic_distribution is suddenly not found anymore")
    res_mic_distribution <- mic_distribution("Vancomycin", "Staphylococcus lugdunensis", res_update_eucast)
 
 
